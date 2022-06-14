566e84065a2a04a343a3d980d52259797fb03c99
robertjosephderouin <robertjosephderouin@gmail.com>
2022-06-14 14:10:36 -0700

    Add intial button code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     this.state = {
       currentPrompt: 0,
       prompts: [
-        {id: 0, title: "Start", text:'This is the start of your journey', next:[1, 1, 1, 1]},
+        {id: 0, title: "Start", text:'This is the start of your journey', nextPrompt:[{next: 1, text: "Continue"}]},
         {id: 1, title: "Middle", text:'This is the middle of your journey', next:[2, 2, 2 ,2]},
         {id: 2, title: "End", text:'This is the end of your journey', next:[0, 0, 0, 0]},
       ],
@@ -25,6 +25,17 @@ class App extends Component {
                 text = {this.state.prompts[this.state.currentPrompt].text}
               ></Card>
           </div>
+          {/* <div className="buttons">
+            <button
+              onClick={() =>
+                this.setState({
+                  //Function goes here.
+                })
+              }
+            >
+            {this.state.prompts[this.state.currentPrompt].next[0].text}
+            </button>
+          </div> */}
       </main>
     )
   }
